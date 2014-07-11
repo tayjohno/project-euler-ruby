d48c6757b4bb3fd493b8f60b518bcd8ce4561338
Taylor Sampson <tsampson@augeogreer.com>
2014-07-11 18:10:57 -0500

    Removed commented out (old) code.

--- TaylorMath.rb
+++ TaylorMath.rb
@@ -19,59 +19,10 @@ module TaylorMath
     return true
 	end
 
-=begin
-  # Returns the smallest prime factor of a given number
-  def self.smallest_prime_factor(number, starting_point = 2)
-    
-    i = starting_point
-    
-    # Check if starting_point is a factor, and if not, continue with the next odd number.
-    return i if self.is_divisible_by(number, i)
-    i += 1 if i.odd?
-    i += 1
-
-    # Loop all odd numbers starting from the smallest to see if any are prime factors (2 is the only even prime)
-    while i <= Math.sqrt(number) do
-      return i if self.is_divisible_by(number, i)
-      i = i + 2
-    end
-
-    return number
-  end
-=end
-
-
   # Returns true if the numerator is evenly divisible by the denominator
   def self.is_divisible_by(numerator, denominator)
     return numerator % denominator == 0
   end
-
-=begin
-  # Returns all prime factors of a number.
-  def self.prime_factors(number)
-    return_array = []
-    i = 2
-    while i <= Math.sqrt(number)  do
-      i = self.smallest_prime_factor(number, i)
-      return_array += [i]
-      number = number / i
-    end
-    return_array += [number] if number != 1
-    return return_array
-  end
-
-  # Returns all prime factors of a number.
-  def self.all_factors(number, starting_point = 2)
-    return_array = [1]
-    i = 2
-    while i <= number  do
-      return_array += [i] if self.is_divisible_by(number, i)
-      i += 1
-    end
-    return return_array
-  end
-=end
-
 
   # Returns all prime factors of a number.
   def self.primes_less_than(number)
