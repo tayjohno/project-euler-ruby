9f3d2db0ee7dd6603724035a77648ab825514264
Taylor Sampson <tsampson@augeogreer.com>
2014-08-07 17:00:14 -0500

    twentythree now runs about a million times faster

--- solutions/023.rb
+++ solutions/023.rb
@@ -1,14 +1,3 @@
-=begin
-
-A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
-
-A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
-
-As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
-
-Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
-
-=end
 require './TaylorMath.rb'
 
 #####################
@@ -16,6 +5,33 @@ require './TaylorMath.rb'
 # ----------------- #
 # Non-Abundant Sums #
 #####################
+
+
+def twentythree(max=28123)
+  a = (0..max).to_a
+  i = 0
+  j = 0
+  all_abundant_numbers = TaylorMath::Abundance.all_abundant_numbers
+  length = all_abundant_numbers.length
+
+  while i < length
+    while j < length
+      sum = all_abundant_numbers[i]+all_abundant_numbers[j]
+      if sum > 28123
+        break
+      else
+        a[sum] = 0
+        j += 1
+      end
+    end
+    i += 1
+    j = i
+  end
+  return TaylorMath.sum(a)
+end
+
+
+=begin THIS WAS A BAD IDEA
 
 def twentythree_refactoring(initial_array=((1..28123).to_a))
   input_array = initial_array
@@ -62,34 +78,7 @@ def twentythree_refactoring(initial_array=((1..28123).to_a))
   end
   return output_array
 end
+=end
 
-# ANSWER: 4179871  -- the below works, but takes almost 30 minutes!
+# ANSWER: 4179871
 
-def twentythree(max=28123)
-  a = (1..max).to_a
-  i = 0
-  j = 0
-  all_abundant_numbers = TaylorMath::Abundance.all_abundant_numbers
-  length = all_abundant_numbers.length
-
-  while i < length
-    puts "(i:#{i},\tj:#{j}) length = #{a.length}"
-    while j < length
-      sum = all_abundant_numbers[i]+all_abundant_numbers[j]
-      if sum > 28123
-        j = length
-      else
-        #if a.include? sum
-          a.delete sum
-        #else
-          #puts "WASTED DELETE"
-        #end
-        j += 1
-      end
-    end
-    i += 1
-    j = i
-  end
-  return TaylorMath.sum(a)
-#  return twentythree(a)
-end
