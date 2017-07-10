7b58325d9badab828e4b1f5638272fc16c0c3acf
Taylor Sampson <tsampson@augeogreer.com>
2017-07-09 21:14:52 -0500

    Requiring Date for 019.rb

--- solutions/019.rb
+++ solutions/019.rb
@@ -1,4 +1,5 @@
 require './lib/taylor_math.rb'
+require 'date'
 
 ########################
 # Problem 19           #
@@ -6,7 +7,7 @@ require './lib/taylor_math.rb'
 # Number Letter Counts #
 ########################
 class Nineteen
-  def initialize(start_date = Date.new(1901, 1, 1), end_date = Date.new(2000, 12, 31))
+  def initialize(start_date = Date.parse('1 Jan 1901'), end_date = Date.parse('31 Dec 2000'))
     @start_date = start_date
     @end_date = end_date
   end
